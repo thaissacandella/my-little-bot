a2daff78332423de1f28996cfe57022682b05157
João Paulo Bassinello <joao.bassinello@tecsinapse.com.br>
2016-03-19 19:06:56 -0300

    URL Shorter

---
+++ src/main/java/br/com/botmotor/service/UrlShortnerService.java
@@ -0,0 +1,15 @@
+package br.com.botmotor.service;
+
+import static com.rosaloves.bitlyj.Bitly.as;
+import static com.rosaloves.bitlyj.Bitly.shorten;
+
+/**
+ * @author "<a href='jpbassinello@gmail.com'>João Paulo Bassinello</a>"
+ */
+public class UrlShortnerService {
+
+	public String shortUrl(String url) {
+		return as("o_v2j1l23am", "R_194d52945187433d8ba71b96a09b9480").call
+				(shorten(url)).getShortUrl();
+	}
+}
